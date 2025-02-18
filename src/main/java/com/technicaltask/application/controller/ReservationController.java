da17578620abcec3751a2fefc50ceb5d0d9368dd
Grzegorz Rombalski <rain.ekk@gmail.com>
2025-02-18 08:42:32 +0100

    test

--- src/main/java/com/technicaltask/application/controller/ReservationController.java
+++ src/main/java/com/technicaltask/application/controller/ReservationController.java
@@ -8,12 +8,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name="Reservation api", description="Api for manipulating reservations")
 @RestController
 @RequestMapping("/api/reservation")
 public class ReservationController {
     @Autowired
     private ReservationService reservationService;
 
+    @Operation(
+            summary = "Create new reservation",
+            description = "Create new reservation",
+            tags = { "reservation", "post" }
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = ReservationDTO.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = { @Content(schema = @Schema()) })
+    })
     @PostMapping
     public ResponseEntity<ReservationDTO> createReservation(@RequestBody ReservationDTO reservation) {
         try {
@@ -24,6 +41,16 @@ public class ReservationController {
         }
     }
 
+    @Operation(
+            summary = "Edit reservation",
+            description = "Edit reservation",
+            tags = { "reservation", "put" }
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = { @Content(schema = @Schema(implementation = ReservationDTO.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", description = "Reservation with given id not found", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = { @Content(schema = @Schema()) })
+    })
     @PutMapping("/{id}")
     public ResponseEntity<ReservationDTO> editReservation(@PathVariable("id") int id, @RequestBody ReservationDTO reservation) {
         try {
@@ -36,6 +63,16 @@ public class ReservationController {
         }
     }
 
+    @Operation(
+            summary = "Delete reservation",
+            description = "Delete reservation",
+            tags = { "reservation", "delete" }
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content =  { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "404", description = "Reservation with given id not found", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = { @Content(schema = @Schema()) })
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteReservation(@PathVariable("id") int id) {
         try {
--- src/main/java/com/technicaltask/application/controller/ReservationsController.java
+++ src/main/java/com/technicaltask/application/controller/ReservationsController.java
@@ -2,6 +2,8 @@ package com.technicaltask.application.controller;
 
 import com.technicaltask.application.dto.ReservationDTO;
 import com.technicaltask.application.service.ReservationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name="Reservations api", description="Api for viewing reservations")
 @RestController
 @RequestMapping("/api/reservations")
 public class ReservationsController {
     @Autowired
     private ReservationService reservationService;
 
+    @Operation(
+            summary = "Retrieve reservations",
+            description = "Get list of reservation objects with id, guest name, date from and date to",
+            tags = { "reservations", "get" }
+    )
     @GetMapping
     public ResponseEntity<List<ReservationDTO>> getAllReservations() {
         return new ResponseEntity<>(this.reservationService.findAll(), HttpStatus.OK);
